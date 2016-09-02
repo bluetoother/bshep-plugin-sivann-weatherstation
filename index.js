c58de8ae807a5120a9577cac70e66fc6b7bcf00e
Hedy Wang <hedywings@gmail.com>
2016-09-02 15:27:05 +0800

    v0.1.0

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 module.exports = {
      gattDefs: {
         service: [
+            { name: 'dInServ', uuid: '0xbb00' },
+            { name: 'aInServ', uuid: '0xbb10' },
             { name: 'weatherServ', uuid: '0xbb80' }
         ],
         characteristic: [
-            { name: 'weatherMeasPeriod', uuid: '0xbb81', params: ['period'], types: ['uint8'] },
-            { name: 'micMeasPeriod', uuid: '0xbb82', params: ['period'], types: ['uint8'] }
+            { name: 'aInConfig', uuid: '0xbb11', params: ['config'], types: ['boolean'] },
+            { name: 'aInMeasPeriod', uuid: '0xbb12', params: ['period'], types: ['uint8'] },
+            { name: 'weatherConfig', uuid: '0xbb81', params: ['config'], types: ['boolean'] },
+            { name: 'weatherMeasPeriod', uuid: '0xbb82', params: ['period'], types: ['uint8'] }
         ]
      },
      examine: function (periph, basicInfo) {
